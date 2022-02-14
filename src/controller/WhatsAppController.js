119252550cb33911ed1dca9c4ea59b670523e417
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-14 13:22:43 -0300

    :busts_in_silhouette: Create Add Contact and Edit Profile buttons

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -7,8 +7,9 @@ class WhatsAppController {
 
             this.elementsPrototype();
             this.loadElements();
+            this.initEvents();
 
-        } // End of constructor
+        } // *** End of constructor
 
     loadElements() {
         this.el = {};
@@ -20,68 +21,109 @@ class WhatsAppController {
         });
     }
 
-    elementsPrototype() {
+    elementsPrototype() { // Self created functions for Prototypes 
 
-        // <=====================||=====================>
-        // Show and Hide
-        Element.prototype.hide = function() {
-            // inside a regular function, the scope is limited to the function, so when 'this' is inside a function, it refers to its Object
-            this.style.display = 'none';
-            return this;
-        }
-        Element.prototype.show = function() {
-            this.style.display = 'block';
-            return this;
-        }
-        Element.prototype.toggle = function() {
-            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
-            return this;
-        }
-
-        // *** Show and Hide
-        // <=====================||=====================>
-
-
-        // <=====================||=====================>
-        // Events (ex click, mouseover etc)
-
-        Element.prototype.on = function(events, fn) {
-            events.split(' ').forEach(event => {
-                this.addEventListener(events, fn);
-            })
-            return this;
-        }
-
-        // <=====================||=====================>
-
-        // <=====================||=====================>
-        // CSS 
-
-        Element.prototype.css = function(styles) {
-            for (let name in styles) {
-                this.style[name] = styles[name];
+            // <=====================||=====================>
+            // Show and Hide
+            Element.prototype.hide = function() {
+                // inside a regular function, the scope is limited to the function, so when 'this' is inside a function, it refers to its Object
+                this.style.display = 'none';
+                return this;
             }
-            return this;
-        }
+            Element.prototype.show = function() {
+                this.style.display = 'block';
+                return this;
+            }
+            Element.prototype.toggle = function() {
+                this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+                return this;
+            }
 
-        Element.prototype.addClass = function(name) {
-            this.classList.add(name);
-            return this;
-        }
-        Element.prototype.removeClass = function(name) {
-            this.classList.remove(name);
-            return this;
-        }
-        Element.prototype.toggleClass = function(name) {
-            this.classList.toggle(name);
-            return this;
-        }
-        Element.prototype.hasClass = function(name) {
-            return this.classList.contains(name);
-        }
+            // *** Show and Hide
+            // <=====================||=====================>
 
-        // *** CSS 
+
+            // <=====================||=====================>
+            // Events (ex click, mouseover etc)
+
+            Element.prototype.on = function(events, fn) {
+                events.split(' ').forEach(event => {
+                    this.addEventListener(events, fn);
+                })
+                return this;
+            }
+
+            // *** Events (ex click, mouseover etc)
+            // <=====================||=====================>
+
+            // <=====================||=====================>
+            // CSS 
+
+            Element.prototype.css = function(styles) {
+                for (let name in styles) {
+                    this.style[name] = styles[name];
+                }
+                return this;
+            }
+
+            Element.prototype.addClass = function(name) {
+                this.classList.add(name);
+                return this;
+            }
+            Element.prototype.removeClass = function(name) {
+                this.classList.remove(name);
+                return this;
+            }
+            Element.prototype.toggleClass = function(name) {
+                this.classList.toggle(name);
+                return this;
+            }
+            Element.prototype.hasClass = function(name) {
+                return this.classList.contains(name);
+            }
+
+            // *** CSS 
+            // <=====================||=====================>
+        } // *** Self created functions for Prototypes
+
+
+
+    initEvents() {
         // <=====================||=====================>
+        // Main buttons
+        this.el.myPhoto.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        })
+
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        })
+
+
+        // *** Main buttons
+        // <=====================||=====================>
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelEditProfile.hide();
+        this.el.panelAddContact.hide();
     }
 
 } // End of class WhatsAppController
